1ae9b143f5ef2ba9fd9f4cf63503776e4e94d750
hungify <nmhungify@gmail.com>
2024-02-10 19:28:50 +0700

    Remove unnecessary aliases

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,8 +1,4 @@
 # Aliases
-alias ls="exa --icons --group-directories-first"
-alias ll="exa --icons --group-directories-first -l"
-alias la="exa --icons --group-directories-first -la"
-
 alias grep='grep --color'
 alias cls='clear'
 
