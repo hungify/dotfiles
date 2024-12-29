db4d29c89e9f06806ae15c4599091eb5c05507e0
hungify <nmhungify@gmail.com>
2024-12-29 13:17:10 +0700

    refactor: enhance setup script with installation checks for dependencies and fonts

--- setup/sketchybar.sh
+++ setup/sketchybar.sh
@@ -3,18 +3,53 @@ CONFIG=${HOME}/.config
 
 echo "Installing Dependencies"
 
-# Packages
-brew install lua
-brew install switchaudio-osx
-brew install nowplaying-cli
+# Check and install Lua if not already installed
+if ! command -v lua &>/dev/null; then
+	brew install lua
+else
+	echo "Lua is already installed"
+fi
 
-brew tap FelixKratz/formulae
-brew install sketchybar
+# Check and install switchaudio-osx if not already installed
+if ! command -v switchaudio-osx &>/dev/null; then
+	brew install switchaudio-osx
+else
+	echo "switchaudio-osx is already installed"
+fi
 
-# Fonts
-brew install --cask sf-symbols
-brew install --cask font-sf-mono
-brew install --cask font-sf-pro
+# Check and install nowplaying-cli if not already installed
+if ! command -v nowplaying &>/dev/null; then
+	brew install nowplaying-cli
+else
+	echo "nowplaying-cli is already installed"
+fi
+
+# Check and install sketchybar if not already installed
+if ! command -v sketchybar &>/dev/null; then
+	brew tap FelixKratz/formulae
+	brew install sketchybar
+else
+	echo "sketchybar is already installed"
+fi
+
+# Check and install fonts if not already installed
+if ! brew list --cask | grep -q 'sf-symbols'; then
+	brew install --cask sf-symbols
+else
+	echo "sf-symbols font is already installed"
+fi
+
+if ! brew list --cask | grep -q 'font-sf-mono'; then
+	brew install --cask font-sf-mono
+else
+	echo "font-sf-mono is already installed"
+fi
+
+if ! brew list --cask | grep -q 'font-sf-pro'; then
+	brew install --cask font-sf-pro
+else
+	echo "font-sf-pro is already installed"
+fi
 
 curl -L https://github.com/kvndrsslr/sketchybar-app-font/releases/download/v2.0.28/sketchybar-app-font.ttf -o $HOME/Library/Fonts/sketchybar-app-font.ttf
 
@@ -25,6 +60,6 @@ echo "Apply sketchybar config"
 
 rm -rf ${CONFIG}/sketchybar
 
-ln -s $(pwd)/sketchybar ${CONFIG}/
+ln -s $(pwd)/../sketchybar ${CONFIG}/
 
 brew services restart sketchybar
