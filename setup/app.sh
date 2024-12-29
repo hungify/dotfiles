d4f25170a0594678beb5ea336594a3a968598982
hungify <nmhungify@gmail.com>
2024-12-29 11:27:16 +0700

    refactor: streamline Node.js installation process and remove deprecated script

--- setup/app.sh
+++ setup/app.sh
@@ -14,8 +14,7 @@ fi
 brew tap homebrew/cask-fonts
 
 # Install apps
-xargs brew install < apps.txt
-
+xargs brew install <apps.txt
 
 # Install bun
 if [[ ! $(bun -v) ]]; then
@@ -27,12 +26,35 @@ else
 	echo "bun already installed"
 fi
 
-
 # Install rust
 if [[ ! $(rustc --version) ]]; then
-    echo "Installing rust"
-    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-    echo "Rust installed"
+	echo "Installing rust"
+	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+	echo "Rust installed"
 else
-    echo "Rust already installed"
+	echo "Rust already installed"
+fi
+
+# Install fnm
+if [[ ! $(fnm --version) ]]; then
+	echo "Installing fnm"
+	brew install fnm
+	echo "fnm installed"
+else
+	echo "fnm already installed"
+fi
+echo "Installing latest LTS version of Node.js"
+
+# Install Node
+if [[ ! $(fnm ls | grep lts) ]]; then
+	read -p "What version of Node.js do you want to install? (lts, or a specific version): " answer
+	if [[ $answer =~ ^[nLl]ts$ ]]; then
+		echo "Installing latest LTS version of Node.js"
+		fnm install --lts
+	else
+		echo "Installing version $answer of Node.js"
+		fnm install $answer
+	fi
+else
+	echo "Latest LTS version of Node.js already installed"
 fi
