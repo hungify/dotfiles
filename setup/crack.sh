bd586bf6e0580706fab110b3db6457b2a04bef02
hungify <nmhungify@gmail.com>
2024-12-29 12:02:25 +0700

    refactor: add EVKey to disable quarantine in setup script

--- setup/crack.sh
+++ setup/crack.sh
@@ -3,3 +3,4 @@
 # Disable quarantine
 sudo xattr -rd com.apple.quarantine /Applications/CleanShot\ X.app
 sudo xattr -rd com.apple.quarantine /Applications/Paste.app
+sudo xattr -rd com.apple.quarantine /Applications/EVKey.app
