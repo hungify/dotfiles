3c53c2b9843f28b1fa85178d0429976bd40e2f6b
hungify <nmhungify@gmail.com>
2024-12-29 12:15:17 +0700

    refactor: add Mos.app to the quarantine removal list in setup script

--- setup/crack.sh
+++ setup/crack.sh
@@ -4,3 +4,4 @@
 sudo xattr -rd com.apple.quarantine /Applications/CleanShot\ X.app
 sudo xattr -rd com.apple.quarantine /Applications/Paste.app
 sudo xattr -rd com.apple.quarantine /Applications/EVKey.app
+sudo xattr -rd com.apple.quarantine /Applications/Mos.app
