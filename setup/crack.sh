8bb9287686b3bbf2331511e6cb7b1f858b747879
hungify <nmhungify@gmail.com>
2025-02-21 23:48:46 +0700

    refactor: update apps list and modify quarantine settings in setup scripts

--- setup/crack.sh
+++ setup/crack.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 # Disable quarantine
-sudo xattr -rd com.apple.quarantine /Applications/CleanShot\ X.app
-sudo xattr -rd com.apple.quarantine /Applications/Paste.app
 sudo xattr -rd com.apple.quarantine /Applications/EVKey.app
 sudo xattr -rd com.apple.quarantine /Applications/Mos.app
